faa36333a347ce5ca10b710301570063bfe8ebac
zacharya19 <zacharya19@gmail.com>
2016-05-29 11:50:03 +0300

    Graphite check ragne by frequency

--- cabot/cabotapp/views.py
+++ cabot/cabotapp/views.py
@@ -153,6 +153,7 @@ class GraphiteStatusCheckForm(StatusCheckForm):
         model = GraphiteStatusCheck
         fields = (
             'name',
+            'frequency',
             'metric',
             'check_type',
             'value',
@@ -769,10 +770,16 @@ def jsonify(d):
 @login_required
 def graphite_api_data(request):
     metric = request.GET.get('metric')
+
+    if request.GET.get('frequency'):
+        mins_to_check = int(request.GET.get('frequency'))
+    else:
+        mins_to_check = None
+
     data = None
     matching_metrics = None
     try:
-        data = get_data(metric)
+        data = get_data(metric, mins_to_check)
     except requests.exceptions.RequestException, e:
         pass
     if not data:
