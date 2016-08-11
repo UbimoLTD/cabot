f7038d046e9543de83ee802bd4e2c13766f3bd01
zacharya19 <zacharya19@gmail.com>
2016-08-12 01:53:42 +0300

    Check all datapoints on graphite

--- cabot/cabotapp/graphite.py
+++ cabot/cabotapp/graphite.py
@@ -83,7 +83,7 @@ def parse_metric(metric, mins_to_check=5):
     all_values = []
     for target in data:
         series = {'values': [
-            float(t[0]) for t in target['datapoints'][-mins_to_check:] if t[0] is not None]}
+            float(t[0]) for t in target['datapoints'] if t[0] is not None]}
         series["target"] = target["target"]
         all_values.extend(series['values'])
         if series['values']:
