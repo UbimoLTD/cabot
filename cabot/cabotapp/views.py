d8889c48aa4bdd7cac04ebcdfb992fb105f168be
zacharya19 <zacharya19@gmail.com>
2016-08-15 16:21:57 +0300

    Add filter to service

--- cabot/cabotapp/views.py
+++ cabot/cabotapp/views.py
@@ -38,8 +38,8 @@ class LoginRequiredMixin(object):
 def subscriptions(request):
     """ Simple list of all checks """
     t = loader.get_template('cabotapp/subscriptions.html')
-    services = Service.objects.all()
-    users = User.objects.filter(is_active=True, rotagroup__isnull=False)
+    services = Service.objects.filter(rotagroup__isnull=False)
+    users = User.objects.filter(is_active=True)
     duty_officers = []
 
     for service in services:
